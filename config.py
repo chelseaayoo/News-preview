e4bba63a9269b8e7a8d5336d35fa169b4d70c9f6
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-01 19:37:24 -0400

    import os

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     parent class configuration
     '''
-    # NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
