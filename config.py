2398f7578c312ac43c1bb9bf5868dff39ac0086e
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-01 18:44:14 -0400

    store source base url

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     parent class configuration
     '''
     # NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-    # CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
 
 
