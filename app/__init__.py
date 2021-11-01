a3a52c7075bb56e3734f7daf68bae0d9902550af
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-01 19:33:34 -0400

    set configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from config import config_options
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
@@ -11,12 +11,12 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    # Initializing flask extensions
-    bootstrap.init_app(app)
+#     # Initializing flask extensions
+#     bootstrap.init_app(app)
 
-# Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+# # Registering the blueprint
+#     from .main import main as main_blueprint
+#     app.register_blueprint(main_blueprint)
 
 # setting config
     from .request import configure_request
