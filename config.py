e29e34b9c4a858c4c7b72a9964b5aba23bd2122b
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-11-01 18:32:58 -0400

    create configuration function

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+    '''
+    parent class configuration
+    '''
+    # NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    # CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Child class development configuration
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
